fbddad71ea7a48c593dfdf5ad1c870016b0a7631
Pepe1612 <patrykpiecuch16@gmail.com>
2021-04-16 16:33:27 +0200

    v3.0

---
+++ src/kaprekar/TestFive.java
@@ -0,0 +1,14 @@
+package kaprekar;
+
+public class TestFive {
+    Kaprekar kap;
+
+    public TestFive() {
+        kap = new Kaprekar();
+    }
+
+    public void print() {
+        int x = 2321;
+        System.out.println(kap.kaprekar(x));
+    }
+}
